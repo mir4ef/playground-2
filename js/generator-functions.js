adeb5c13df6fc81cd739894ac8ac68974139a73c
Miro <mir4ef@gmail.com>
2019-10-18 21:19:38 -0400

    added generator function example

--- js/generator-functions.js
+++ js/generator-functions.js
@@ -1,9 +1,17 @@
-/**
- * Created by miroslav.georgiev on 8/9/16.
- */
+'use strict';
 
-(function () {
-    'use strict';
+async function* f() {
+    for (let i = 1; i < 6; i++) {
+        yield i;
+    }
 
+    // for await (const val of [1, 2, 3, 4, 5]) {
+    //     yield val;
+    // }
+}
 
+(async function () {
+    for await (const val of f()) {
+        console.log(`${val}/5`);
+    }
 })();
